1c331fd768cf31637f635901f1efc3571274e97d
a1093370 <125646445+gigaliu@users.noreply.github.com>
2024-04-18 10:20:16 +0800

    Create index.php

---
+++ php2024midtern/index.php
@@ -0,0 +1,27 @@
+<html>
+<meta charset="utf8">
+
+</html>
+<center><h1 style="font-size:35pptx">高大資管論文投稿系統</h1><center>
+
+<form action ="check.php" method="post">
+
+請選擇您的角色<br/><br/>
+<select role="sRole[]" multiple>
+<option value="Chair">Chair
+<option value="Reviewer">Reviewer
+<option value="Author">Author
+</select>
+<br/><br/><br/><br/><br/>
+
+帳號:<input type="text" name="uId"><br/><br/>
+密碼:<input type="password" name="uPWD"><br/><br/>
+
+
+
+<input type="submit">
+
+</form>
+
+<?php
+?>
