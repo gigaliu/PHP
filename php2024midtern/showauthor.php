19b50c08f6c653f908b834b10fa79d0d1ebaa2a7
a1093370 <125646445+gigaliu@users.noreply.github.com>
2024-04-18 10:25:38 +0800

    Create showauthor.php

---
+++ php2024midtern/showauthor.php
@@ -0,0 +1,15 @@
+<html>
+<meta charset="utf8">
+
+</html>
+<center><h1 style="font-size:35pptx">Author您好，歡迎進入論文評論網頁</h1><center>
+
+論文標題:<br/><br/>
+<input type="text" name="sTitle" ><br/><br/>
+作者姓名:<input type="text" name="sAuthorname" ><br/><br/>
+作者Email:<input type="text" name="sAuthoremail" ><br/><br/>
+
+論文摘要:
+<textarea name="sComment" rows="25" cols="100"></textarea>
+<br/><br/>
+<input type="submit">
